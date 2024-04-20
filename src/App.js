1ce85adcc0eebb76de227f9809454adc44b4039b
mohanhasti <mohankrish.97@gmail.com>
2024-04-19 17:33:26 -0700

    modified the routes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { LinkedInCallback } from "react-linkedin-login-oauth2";
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} />  */}
-      <Route exact path="/linkedin" element={<LinkedInCallback/>} />
-      <Route path="/" element={<LinkedInPage/>} />
+      <Route path="/" element={<Home />} /> 
+      <Route exact path="/linkedin/callback" element={<LinkedInCallback/>} />
+      <Route path="/linkedin" element={<LinkedInPage/>} />
 
     </Routes>
   );
