db1e7acc4ddd892bcc2f04645313dc4d09ae62f4
mohanhasti <mohankrish.97@gmail.com>
2024-04-19 17:30:17 -0700

    Added Linkedin API Authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import LinkedInPage from "./utils/LinkedInPage";
+import { LinkedInCallback } from "react-linkedin-login-oauth2";
 import Home from "./main.js"
 import PromptPage from "./pages/PromptPage/PromptPage";
 
 function App() {
   return (
     <Routes>
+      <Route exact path="/linkedin" element={<LinkedInCallback/>} />
+      {/* <Route path="/" element={<LinkedInPage/>} /> */}
       <Route path="/" element={<Home />} />
       <Route path="/prompt" element={<PromptPage />} />
     </Routes>
