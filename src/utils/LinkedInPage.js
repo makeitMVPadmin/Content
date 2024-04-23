7e541c634bd22976ffba638218a233ef3cf929bd
mohanhasti <mohankrish.97@gmail.com>
2024-04-23 14:27:37 -0700

    Feature: added a post button

--- src/utils/LinkedInPage.js
+++ src/utils/LinkedInPage.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
+import {Button} from "react-dom";
 import { useLinkedIn } from "react-linkedin-login-oauth2";
 import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
 import { getAccessTokenData } from "./LinkedInUtils";  
@@ -27,7 +28,7 @@ const LinkedInPage = () =>{
   const [code, setCode] = React.useState("");
   const [errorMessage, setErrorMessage] = React.useState("");
   const [userName, setUserName] = React.useState("");
-  // const [accessToken, setAccessToken] = React.useState("");
+  let accessToken = '';
 
 
   if (code){
@@ -36,45 +37,29 @@ const LinkedInPage = () =>{
         const accessTokenData = await getAccessTokenData(code);
         console.log("accessTokenData: ", accessTokenData);
         // setAccessToken(accessTokenData.accessToken);
+        accessToken = accessTokenData.accessToken;
       }catch (error) {
         console.error("Error fetching access token:", error);
       }
     }
     getAccessToken();
   }
+  if(accessToken !== ''){
+    console.log("Accesstoken", accessToken);
+
+  }
 
 
   return (
     <div>
-      <img
+      <button
         onClick={linkedInLogin}
-        src={linkedin}
-        alt="Log in with Linked In"
-        style={{ maxWidth: "180px", cursor: "pointer" }}
-      />
+        title="Post"
+        style={{cursor: "pointer" }}
+      >
+        Post
+      </button>
 
-      {/* {!code && <div>No code</div>} */}
-      {/* {code && ( */}
-        {/* <div>
-          <div>Authenticated: Authorization Code: {code}</div>
-          <div>
-            Follow{" "}
-            <a
-              target="_blank"
-              href="https://docs.microsoft.com/en-us/linkedin/shared/authentication/authorization-code-flow?context=linkedin%2Fconsumer%2Fcontext&tabs=HTTPS#step-3-exchange-authorization-code-for-an-access-token"
-              rel="noreferrer"
-            >
-              this
-            </a>{" "}
-            to continue
-          </div>
-          <div>
-            UserName = {userName}
-          </div>
-        </div> */}
-      {/* )} */}
-      {/* {!accessToken && <div>No accessToken</div>} */}
-      {/* {accessToken && <div>Access token: {accessToken}</div>} */}
       {errorMessage && <div>{errorMessage}</div>}
     </div>
   );
