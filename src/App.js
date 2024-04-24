d0d719ec28f4f2bb3b298b947c91c57f0cef7218
Eileen Burdekin <eburdekin@proton.me>
2024-04-23 23:38:23 -0700

    add /mockpost route to preview new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./main.js"
+import Home from "./main.js";
 import PromptPage from "./pages/PromptPage/PromptPage";
+import MockLinkedInPost from "./components/MockLinkedInPost/MockLinkedInPost.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/prompt" element={<PromptPage />} />
+      <Route path="/mockpost" element={<MockLinkedInPost />} />
     </Routes>
   );
 }
