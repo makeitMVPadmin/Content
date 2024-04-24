9efafac4be1cef84c14e2769e74976e6e6001640
Eileen Burdekin <eburdekin@proton.me>
2024-04-23 23:38:02 -0700

    undo main.js change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-// import UserList from "./components/UserList/UserList";
+import UserList from "./components/UserList/UserList";
 import "./styles/partials/_global.scss";
 
 const Home = () => {
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home">
       <h1 className="home__title"> Welcome to COMMIT AI!</h1>
       <Link to="/prompt">Prompt the AI here</Link>
-      {/* <UserList /> */}
+      <UserList />
     </div>
   );
 };
